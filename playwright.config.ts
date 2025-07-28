13b788906ae9b27c6e91762c8e5c0d5a6ce139d4
a915284 <maksym.dotsenko@eviden.com>
2025-07-28 17:31:11 +0200

    fix: report configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-        ['html'],
+        ['html', {outputFolder: 'playwright-results/html-report'}],
         ['json', {outputFile: 'playwright-results/report.json'}],
         ['list'],
     ],
